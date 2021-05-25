a6b321494aec1e6ff92a0517b1d8fd1e98c47260
luxferrew <luxferrew@gmail.com>
2021-05-25 18:20:15 +0800

    feat: add evethandler

--- src/script.js
+++ src/script.js
@@ -1,69 +1,77 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import * as dat from "dat.gui";
+import * as THREE from "three";
+import "./style.css";
+
+// Texture Loader
+const textureLoader = new THREE.TextureLoader();
 
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+//Object
+const geometry = new THREE.PlaneGeometry(1, 1.3);
+for (let i = 0; i < 4; i++) {
+  const material = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(`/${i}.jpg`),
+  });
 
-// Materials
-
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
-
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+  const img = new THREE.Mesh(geometry, material);
+  img.position.set(Math.random() + 0.3, i * -1.8);
+  scene.add(img);
+}
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
+
+gui.add(camera.position, "y").min(-5).max(5).step(0.1);
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
@@ -73,33 +81,39 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//Mouse
+window.addEventListener("wheel", onmousewheel);
+let y = 0,
+  position = 0;
+function onmousewheel(e) {
+  y = e.deltaY * 0.0007;
+}
 
 /**
  * Animate
  */
 
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    const elapsedTime = clock.getElapsedTime()
+  // Update Orbital Controls
+  // controls.update()
+  position += y;
+  y *= 0.9;
+  camera.position.y = position;
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+  // Render
+  renderer.render(scene, camera);
 
-    // Update Orbital Controls
-    // controls.update()
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+tick();
